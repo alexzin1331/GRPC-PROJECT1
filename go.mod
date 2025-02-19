e03863ccc95df5ed5081d38ac1afdde11af80360
Alex <alex@gmail.com>
2025-02-19 15:40:58 +0300

    add files

---
+++ go.mod
@@ -0,0 +1,3 @@
+module Project_gRPC_SSO_Auth(1)
+
+go 1.23
---
+++ sso/internal/app/app.go
@@ -0,0 +1,22 @@
+package grpcapp
+
+import (
+	"log/slog"
+	grpcapp "sso_module/internal/app/grpc"
+	"time"
+)
+
+type App struct {
+	GRPCSrv *grpcapp.App
+}
+
+func New(log *slog.Logger, grpcPort int, storagePath string, tokenTTL time.Duration) *App {
+	//TODO: init storage
+	//TODO: init auth service
+
+	grpcApp := grpcapp.New(log, grpcPort)
+
+	return &App{
+		GRPCSrv: grpcApp,
+	}
+}
